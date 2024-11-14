de371d6eb6e9326df851d1d45ccf4ce0188b0183
kissagnesa <sindzse88@gmail.com>
2024-11-14 18:17:44 +0100

    c

--- src/ChessList.jsx
+++ src/ChessList.jsx
@@ -5,12 +5,24 @@ export const ChessList=()=>{
     const[chesses,setChesses]=useState([]);
     const [isPending,setPending]=useState(false);
 
-    useEffect((){
+    useEffect(()=> {
         setPending(true);
         fetch ("https://chess.sulla.hu/chess")
         .then ((valasz)=>valasz.json())
         .then((sakkosok)=>setChesses(sakkosok))
         .cath((hiba)=>console.log(hiba))
-        .finally(()=>setPending(false));
-    }, [])
+        .finally(()=>setPending(false)); //a finally ág mindenképp lefut a promise-ok közül
+    }, []);
+
+    return(
+        <div className="p-5 m-auto text-center content bg-ivory">
+            {isPending ? (
+                <div className="spinner-border text-danger"></div>
+            ):(
+                <div>
+                    <h2>Sakkozók</h2>
+                </div>
+            )}
+        </div>
+    )
 }
